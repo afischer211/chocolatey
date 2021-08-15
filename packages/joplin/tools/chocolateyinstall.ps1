9aa4f9623c42ccceb56489eae12cdacf54c9d37c
Alexander Fischer <fischer.alexander@web.de>
2021-08-15 16:04:34 +0200

    modified for version 2.2.7

--- packages/joplin/tools/chocolateyinstall.ps1
+++ packages/joplin/tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@
 # 2018 foo.li systeme + software, afischer211
 $ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version               = '2.0.11'
+$version               = '2.2.7'
 $packageSearch         = 'Joplin*'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  file          = Join-Path $toolsDir 'Joplin-Setup-2.0.11.exe'
+  file          = Join-Path $toolsDir 'Joplin-Setup-2.2.7.exe'
   softwareName  = $packageSearch
   silentArgs    = '/ALLUSERS=1 /S'
   validExitCodes= @(0)
