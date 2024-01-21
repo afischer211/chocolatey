2d5eabc40bd3470a2262d1e0b4b7bea340576b61
Alexander Fischer <fischer.alexander@web.de>
2024-01-21 23:24:30 +0100

    reworked update-script

--- packages/joplin/update.ps1
+++ packages/joplin/update.ps1
@@ -3,8 +3,9 @@
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*File\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName32)'"
-            "(?i)(^.*version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
+#            "(?i)(^\s*file\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName32)'"
+            "(?i)(^.*version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
+			"(?i)(^.*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
         ".\legal\VERIFICATION.txt" = @{
             "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
