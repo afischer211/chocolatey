66251d2b8a9e62ae686690401e4680dd3bb659ff
Dmitry Romanenko <Dmitry@Romanenko.in>
2024-03-03 14:52:26 -0500

    Upgrade joplin to 2.14.17

--- packages/joplin/tools/chocolateyinstall.ps1
+++ packages/joplin/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 # 2018 foo.li systeme + software, afischer211
 $ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version               = '2.13.15'
+$version               = '2.14.17'
 $packageSearch         = 'Joplin*'
 $installerFileName     = 'Joplin-Setup-' + $version + '.exe'
 $file          = Join-Path $toolsDir $installerFileName
 $installerType = 'exe'
 $packageName   = 'joplin'
 $url 		   = 'https://github.com/laurent22/joplin/releases/download/v' + $version + '/' + $installerFileName
-$checksum      = '8DB589A1D34F690A417E4ADB1AF51E36AFAE1B819217CBD3D14DC06B8304167F'
+$checksum      = '854D77B8210A7B15AFA8E6504423DA723EA720D8651B0BE7FB233BA88BCDA45F'
 $checksumType  = 'sha256'
 
 $packageArgs = @{
