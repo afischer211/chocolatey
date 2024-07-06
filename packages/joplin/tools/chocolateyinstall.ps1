2cbe2c012cac946f37bacb40859646a1ae7e8b26
Christian Schlotter <schlotter@users.noreply.github.com>
2024-07-06 11:50:14 +0200

    modified for version 3.0.12

--- packages/joplin/tools/chocolateyinstall.ps1
+++ packages/joplin/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@
 # 2018 foo.li systeme + software, afischer211
 $ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version               = '2.14.22'
+$version               = '3.0.12'
 $packageSearch         = 'Joplin*'
 $installerFileName     = 'Joplin-Setup-' + $version + '.exe'
 $file          = Join-Path $toolsDir $installerFileName
 $installerType = 'exe'
 $packageName   = 'joplin'
 $url 		   = 'https://github.com/laurent22/joplin/releases/download/v' + $version + '/' + $installerFileName
-$checksum      = '162AD99AD96CF8FBB3296C11D5088675D9A5F66FEFF8FDD6C4B37FAE2BBB59A8'
+$checksum      = '5B80D791FB4ED3D87A660E7A969797B81BD2933F9D0F3C3805671A7EA946E76C'
 $checksumType  = 'sha256'
 
 $packageArgs = @{
